87232b1f250746faf2bd97d7de618f2cc5b2a154
E-RKP <edobackup111@gmail.com>
2025-05-04 17:23:57 +0200

    version 6

--- data.js
+++ data.js
@@ -98,5 +98,114 @@ for (i=0; i<countries.length;i++){
 
 console.log(land)
 
+let newCountriess = []
+
+for (i=0; i<countries.length; i++){
+
+  newCountriess[countries.length-1-i] = countries[i].toUpperCase()
+  
+}
+
+console.log(newCountriess)
+
+let asterisco = '#'
+console.log(asterisco)
+for (i=0; i < 5; i++){
+
+  asterisco = asterisco + ' #'
+  console.log(asterisco)
+}
 
 
+let rgb = 0
+let rgbArr = []
+for (i=0; i<3; i++){
+
+  rgb = Math.floor(Math.random()*256)
+  rgbArr.push(rgb)
+  
+}
+
+
+console.log(`RGB: ${rgbArr}`)
+
+
+let mern =''
+
+for (i=0; i<mernStack.length; i++){
+
+  mern = mern+mernStack[i].slice(0,1)
+}
+
+console.log(mern)
+
+
+const fullStack = [
+  ['HTML', 'CSS', 'JS', 'React'],
+  ['Node', 'Express', 'MongoDB']
+]
+
+
+for (i=0; i<fullStack.length; i++){
+
+  
+
+  for (j=0; j < fullStack[i].length; j++){
+    console.log(fullStack[i][j])
+  }
+
+ 
+  
+
+    
+}
+
+
+let casual = [
+
+  [0,4,6,7,8,9,'yu6'],
+  ['ciao', 'uff', 'jjj'],
+  ['patagonia', 'lol']
+]
+
+let plus = ''
+
+for (i=0; i < casual.length; i++){
+
+  for (j=0; j < casual[i].length; j++){
+     if (casual[i][j] > 0 && typeof casual[i][j] === typeof 1){
+      plus = plus + casual[i][j]
+    }
+  }
+    
+}
+
+console.log(plus)
+
+
+
+let casuale = [
+
+  [0,4,6,7,8,9,'yu6'],
+  ['ciao', 'uff', 'jjj'],
+  ['patagonia', 'lol'],
+  ['enieiv', 12]
+]
+
+let somma = 0
+for (i=0; i < casuale.length; i++){
+
+  for (j=0; j < casuale[i].length; j++){
+
+    if (typeof casuale[i][j] == typeof 1){
+      somma = somma+ casuale[i][j]
+    }
+  }
+
+}
+
+if (somma > 10){
+  console.log(`La somma ${somma} è maggiore di 10`)
+}else{
+  console.log(`La somma ${somma} è minore di 5`)
+}
